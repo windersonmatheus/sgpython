fd1ff46ade371c4eebda79bbf5a0cb4682a096fd
windersonmatheus <wm587sr@gmail.com>
2026-04-12 11:20:04 -0300

    Create atividade2if.py

---
+++ atividade2if.py
@@ -0,0 +1,8 @@
+flechas = 50
+inimigo = 50
+if flechas > inimigo:
+    print ("dano efetivo inimigo derrotado")
+if flechas < inimigo:
+    print("dano insuficiente voce morreu")
+if flechas == inimigo:
+    print ("voce venceu a batalha porem ficou sem flechas")
