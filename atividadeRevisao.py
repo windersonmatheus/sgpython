9f872c6089f279ad2169939d167aefc6606dfc70
windersonmatheus <wm587sr@gmail.com>
2026-04-12 09:15:22 -0300

    Create atividadeRevisao.py

---
+++ atividadeRevisao.py
@@ -0,0 +1,9 @@
+minha_idade= 14
+texto_minha_idade="minha idade "
+#print( texto_minha_idade+ str(minha_idade))
+minha_altura="1.75"
+minha_idade += float(minha_altura)
+#print("e minha altura "+ str(minha_altura)+ " somada a minha idade resulta em "+str(minha_idade))
+bole_altura = True
+bole_idade = False
+print(" minha altura esta "+ str(bole_altura)+ " e minha idade esta "+ str(bole_idade))
