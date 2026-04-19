e7062a2307aa693244a86626c7cd786d1ed745fc
windersonmatheus <wm587sr@gmail.com>
2026-04-19 10:45:57 -0300

    Create atividade3if.py

---
+++ atividade3if.py
@@ -0,0 +1,13 @@
+nublado = True
+chovendo = True
+if chovendo:
+  print ("ta chovendo melhor levar o guarda chuva")
+else :
+  # caso nao estiver chovendo mas estiver nublado levarei o guarda chuva
+  if nublado == True:
+    # caso esteja nublado e nao esteja chovendo o certo seria levar o guarda chuva
+    print ("eita esta nublado melhor nao arriscar,vou pegar o guarda chuva")
+  else:
+    print ("vou arriscar sair sem guarda chuva e ver no que vai dar")
+
+
