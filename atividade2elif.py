09a1cb8108772b322da5e95f92be26465be952ce
windersonmatheus <wm587sr@gmail.com>
2026-05-08 19:50:31 -0300

    Create atividade2elif.py

---
+++ atividade2elif.py
@@ -0,0 +1,14 @@
+
+nota = float(input("digite sua nota: "))
+if nota >=9:
+    print("A")
+elif nota >=8:
+    print("B")
+elif nota >=7:
+    print("C")
+elif nota >=7:
+    print("C")
+elif nota >=6:
+    print("D")
+else:
+    print("F")
