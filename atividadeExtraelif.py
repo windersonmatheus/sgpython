c162dba5bb0406cefcd2669b162d46a6fdde60bb
windersonmatheus <wm587sr@gmail.com>
2026-05-15 20:59:40 -0300

    Create atividadeExtraelif.py

---
+++ atividadeExtraelif.py
@@ -0,0 +1,8 @@
+municoes = int(input("quantas balas voce tem: "))
+faca =input("voce utilizou a faca: ")
+silencio = input("voce foi silencioso: ")
+alvos = ("quantos alvos voce tem: ")
+if alvos == 1 and municoes > 0 and silencio=="sim":
+    print("tiro certeiro na cabeca silencioso e presciso")
+    
+
