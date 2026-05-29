d7756174a61ae6ba0387d643f209ad805908c410
windersonmatheus <wm587sr@gmail.com>
2026-05-29 20:15:02 -0300

    Create atividade2while.py

---
+++ atividade2while.py
@@ -0,0 +1,18 @@
+import random
+numeroMaximo = int(input("vamos adivinhar de 1 ate que numero? "))
+chances = int(input("quantas chances voce tem? "))
+numerodesconhecido = 0
+numerocerto = random.randint(1,numeroMaximo)
+while numerocerto != numerodesconhecido and chances > 0 :
+    numerodesconhecido = int(input("digite um numero: "))
+    if numerocerto > numerodesconhecido:
+        print ("o correto e maior")
+        chances -= 1
+    elif numerocerto < numerodesconhecido:
+        print ("o correto e menor")
+        chances -= 1
+    else :
+        print ("na lata campeao")
+
+if chances == 0:
+    print ("poxa amigo nao foi dessa vez,acabaram suas chances")
